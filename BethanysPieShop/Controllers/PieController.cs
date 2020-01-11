f90e3cba50f2806ea5db7e2b0b7bc83dba506f4c
Accelerator <puteranikdimas@yahoo.com.sg>
2020-01-11 16:35:37 +0800

    Added readme and pies

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -23,7 +24,12 @@ namespace BethanysPieShop.Controllers
 
         public ViewResult List()
         {
-            return View(_pieRepository.AllPies);
+            PiesListViewModel piesListViewModel = new PiesListViewModel();
+            piesListViewModel.Pies = _pieRepository.AllPies;
+
+            piesListViewModel.CurrentCategory = "Cakes";
+
+            return View(piesListViewModel);
         }
 
         // GET: /<controller>/
---
+++ BethanysPieShop/Readme.md
@@ -0,0 +1,6 @@
+﻿# ASP.NET Tutorial
+
+## Instructions
+```
+Load solution in VS Code 2019 with ASP.NET Core 3 installed and press ctrl + f5 to build and run it on a web browser. 
+```
